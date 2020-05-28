68aad96f7c837b40881c0ec346c4f89ffeb65241
beuady <beuady@zy.com>
2020-05-28 11:14:02 +0800

    学习总结

--- LC5.28.playground/Contents.swift
+++ LC5.28.playground/Contents.swift
@@ -1,5 +1,5 @@
 //iamport UIKit
-
+import Foundation
 
 let s = "2"
 Int(s)
@@ -7,7 +7,30 @@ let s2 = "a"
 Int(s2)
 
 print("hello world")
+let pattern = #"[a-z,A-Z]"#
+let ch = "a"
+NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: ch)
 
+/*
+ /// https://leetcode-cn.com/problems/decode-string/
+ 学习总结:
+    swift基础:
+    swift的判断字符是否为数字，用类型转换+optional判断，判断字符是否为字母，用ascii码，[a,z] = (64, 91)和 [A,Z] = (96,123)。
+    其实也可以用正则表达式
+    算法方面:
+        递归思想
+        关键是 res + getStr()
+        边界条件是 pr == S.count || S[pr] == "]"
+        转码核心逻辑:
+            let str = getStr()
+            while repeatTime!=0{
+                str += str
+                repeatTime -= 1
+            }
+            res = res + str
+        另外一个很重要的递归出口因子，字符索引指针pr一定是 0..<S.count的
+ 
+ */
 class Solution394 {
     var S:[Character] = [Character]()
     var pr:Int = 0 // index pointer
@@ -53,8 +76,10 @@ class Solution394 {
                 res += str
             }
             
-        }else if isLetter(cur) {
+        }else if isLetter2(cur) {
             res = res + String(cur)
+            pr += 1
+        }else{
             pr += 1
         }
 
@@ -66,7 +91,19 @@ class Solution394 {
         if (ascii > 64 && ascii < 91) || (ascii > 96 && ascii < 123) {
             return true
         }
+        
         return false
     }
+    
+    
+    
+    func isLetter2(_ char:Character) -> Bool {
+        let pattern = #"[a-z,A-Z]"#
+        if NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: String(char)){
+            return true
+        }
+        return false
+    }
+    
 }
-Solution().decodeString("3[a]2[bc]")
+Solution394().decodeString("3[a]2[bc]")
