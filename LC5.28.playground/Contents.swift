8e4490ecffdd25660b2ca5fcafe6f042cd60fb16
beuady <beuady@zy.com>
2020-06-06 11:10:52 +0800

    提交当天

--- LC5.28.playground/Contents.swift
+++ LC5.28.playground/Contents.swift
@@ -1,15 +1,160 @@
 //iamport UIKit
 import Foundation
 
-let s = "2"
-Int(s)
-let s2 = "a"
-Int(s2)
+//for i in stride(from: 0, to: 5, by: 1){
+//    print(i)
+//}
+//print("...")
+//for i in stride(from: 0, through: 5, by: 1){
+//    print(i)
+//}
+/**
+ 学习总结:
+ 1.一定要优先注意边界，包括起始边界，还有终点边界
+ 2.关注算法性能问题,如果发现O(n^2)基本上是不会pass的解法。
+ 3.要仔细考虑题目给的目标区间,比如整数这个条件，就需要考虑负数
+ 4.一旦产出思考逻辑过渡复杂的时候，其实极大可能思路是错误的。要学会不断评估已有算法的逻辑是否清晰和简明
+ */
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        if nums.count < 2 {
+            return nums.count
+        }
+        let sortNums = nums.sorted() // O(logn)
+        var res = 0
+        let n = sortNums.count
+        var l=0, repeatCount = 0
+        for i in stride(from: 1, to: n, by: 1) {
+            if sortNums[i-1] == sortNums[i] {
+                repeatCount += 1
+            }else {
+                if sortNums[i-1] + 1 != sortNums[i] {
+                    l = i
+                    repeatCount = 0
+                }
+            }
+            res = max(res, i - l + 1 - repeatCount)
+        }
+        
+        return res
+    }
+    
+    func testCase() {
+        longestConsecutive([100, 4, 200, 1, 3, 2]) == 4
+        longestConsecutive([0,0]) == 1
+        longestConsecutive([1,2,0,1]) == 3
+        longestConsecutive([0,0,-1]) == 2
+        longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]) == 7
+    }
+    
+    
+}
+Solution().testCase()
 
-print("hello world")
-let pattern = #"[a-z,A-Z]"#
-let ch = "a"
-NSPredicate(format: "SELF MATCHES %@", pattern).evaluate(with: ch)
+/*
+class Solution {
+    var sortNums:[Int]!
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        if nums.count < 2 {
+            return nums.count
+        }
+        let sorts = nums.sorted() // O(logn)
+        sortNums = sorts //O(logm+m)
+        var res = 0
+        
+        var l = 0, r = 0, n = sortNums.count
+        while r < n {
+            let (con,repeatCnt) = isContinuous(l, r)
+            if !con {
+                l = r
+            }else{
+                res = max(res, r-l+1 - repeatCnt)
+            }
+            
+            r += 1
+        }
+        
+        return res
+    }
+    
+    func isContinuous(_ l:Int, _ r:Int) -> (Bool,Int) {
+        var repeatCount = 0
+        if l == r {
+            return (true,repeatCount)
+        }
+        for i in stride(from: l+1, through: r, by: 1) {
+            if sortNums[i-1] == sortNums[i] {
+                repeatCount += 1
+                continue
+            }else if sortNums[i-1] + 1 != sortNums[i] {
+                return (false,repeatCount)
+            }
+        }
+        
+        return (true,repeatCount)
+    }
+    
+}
+ */
+
+/**
+ 面试题29. 顺时针打印矩阵
+ https://leetcode-cn.com/problems/shun-shi-zhen-da-yin-ju-zhen-lcof/
+ */
+class Solution29 {
+    func spiralOrder(_ matrix: [[Int]]) -> [Int] {
+        if matrix.count == 0 || matrix.first?.count == 0 {
+            return []
+        }
+        let rows = matrix.count
+        let cols = matrix.first!.count
+        let n = rows * cols
+        var res = [Int]()
+        var visited = [[Int]](repeating: [Int](repeating: 0, count: cols), count: rows)
+        let dirs = [[1,0], [0, 1], [-1,0], [0, -1]]
+        var dir = 0
+        var col = 0, row = 0
+        for _ in 0 ..< n {
+            let num = matrix[row][col]
+            visited[row][col] = 1
+            res.append(num)
+
+            let nextRow = row + dirs[dir][1]
+            let nextCol = col + dirs[dir][0]
+
+            if nextRow>=rows || nextCol>=cols || nextCol < 0 || nextRow < 0 || visited[nextRow][nextCol]==1 {
+                dir = (dir + 1)%4
+            }
+            row = row + dirs[dir][1]
+            col = col + dirs[dir][0]
+        }
+        return res
+    }
+}
+let tmp = Solution29().spiralOrder([[1,2,3],[4,5,6],[7,8,9]])
+print(tmp)
+tmp == [1,2,3,6,9,8,7,4,5]
+
+/// 238. 除自身以外数组的乘积
+/// https://leetcode-cn.com/problems/product-of-array-except-self/
+class Solution238 {
+    func productExceptSelf(_ nums: [Int]) -> [Int] {
+        var res = [Int](repeating: 1, count: nums.count)
+        var Left = 1
+        var Right = 1
+        for l in 0..<nums.count {
+            res[l] *= Left
+            Left = Left * nums[l]
+            
+            let r = nums.count-1-l
+            res[r] *= Right
+            Right = Right * nums[r]
+            
+        }
+        return res
+    }
+}
+Solution238().productExceptSelf([1,2,3,4]) == [24,12,8,6]
 
 /*
  /// https://leetcode-cn.com/problems/decode-string/
